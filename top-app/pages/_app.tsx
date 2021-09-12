fac0ba4b203f9fdb920ec8566160b2c686ddf8f0
dvwhitem <dvwhitem@gmail.com>
2021-09-12 21:24:34 +0300

    add fonts

--- top-app/pages/_app.tsx
+++ top-app/pages/_app.tsx
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import {Fragment} from 'react'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: any): JSX.Element {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: any): JSX.Element {
+    return (<Fragment>
+        <Head>
+            <title>Top App</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+                  rel="stylesheet"/>
+        </Head>
+        <Component {...pageProps} />
+    </Fragment>
+)
 }
 
 export default MyApp
